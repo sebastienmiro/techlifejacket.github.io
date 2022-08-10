01686393aaba0e9df57c75c748923e19a4b401cd
sebastienmiro <5745300+techlifejacket@users.noreply.github.com>
2022-08-10 22:58:23 +0200

    Added persistent dark mode using local storage :-)

--- assets/js/skin-switch.js
+++ assets/js/skin-switch.js
@@ -1,6 +1,34 @@
-$(function() {
-  $('#change-skin').on('click', function () {
-    $("body").toggleClass("page-dark-mode");
-    BeautifulJekyllJS.initNavbar();
-  });
-});
+var darkMode;
+
+if (localStorage.getItem('dark-mode')) {  
+  // if dark mode is in storage, set variable with that value
+  darkMode = localStorage.getItem('dark-mode');
+} else {  
+  // if dark mode is not in storage, set variable to 'light'
+  darkMode = 'light';  
+}
+
+if (darkMode == 'dark') {
+  // if the above is 'dark' then apply 'page-dark-mode' class to body
+  $("body").toggleClass("page-dark-mode");  
+  // Toggle mode on click event
+  $(function() {
+    $('#change-skin').on('click', function () {
+      $("body").toggleClass("page-dark-mode");
+      BeautifulJekyllJS.initNavbar();
+      darkMode = 'light';
+      localStorage.setItem('dark-mode', 'light'); 
+    });
+   });
+}
+else {
+  $(function() {
+    $('#change-skin').on('click', function () {
+      $("body").toggleClass("page-dark-mode");
+      darkMode = 'dark';
+      localStorage.setItem('dark-mode', 'dark'); 
+      BeautifulJekyllJS.initNavbar();
+    });
+   });
+   
+}
