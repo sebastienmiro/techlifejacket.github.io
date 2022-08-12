b949f5058b31a57d0a5483f4692c412e468a4f97
sebastienmiro <5745300+techlifejacket@users.noreply.github.com>
2022-08-12 21:57:30 +0200

    Refactoring persistent dark mode using Dean's code which is way shorter than mine

--- assets/js/skin-switch.js
+++ assets/js/skin-switch.js
@@ -1,34 +1,10 @@
-var darkMode;
-
-if (localStorage.getItem('dark-mode')) {  
-  // if dark mode is in storage, set variable with that value
-  darkMode = localStorage.getItem('dark-mode');
-} else {  
-  // if dark mode is not in storage, set variable to 'light'
-  darkMode = 'light';  
-}
-
-if (darkMode == 'dark') {
-  // if the above is 'dark' then apply 'page-dark-mode' class to body
-  $("body").toggleClass("page-dark-mode");  
-  // Toggle mode on click event
-  $(function() {
-    $('#change-skin').on('click', function () {
-      $("body").toggleClass("page-dark-mode");
-      BeautifulJekyllJS.initNavbar();
-      darkMode = 'light';
-      localStorage.setItem('dark-mode', 'light'); 
-    });
-   });
-}
-else {
-  $(function() {
-    $('#change-skin').on('click', function () {
-      $("body").toggleClass("page-dark-mode");
-      darkMode = 'dark';
-      localStorage.setItem('dark-mode', 'dark'); 
-      BeautifulJekyllJS.initNavbar();
-    });
-   });
-   
-}
+$(function() {
+  $('#change-skin').on('click', function () {
+    $("body").toggleClass("page-dark-mode");
+    localStorage.setItem('bj-dark-mode', $("body").hasClass("page-dark-mode"));
+    BeautifulJekyllJS.initNavbar();
+  });
+  if (localStorage.getItem('bj-dark-mode') === 'true') {
+    $('#change-skin').trigger('click');
+  }
+});
